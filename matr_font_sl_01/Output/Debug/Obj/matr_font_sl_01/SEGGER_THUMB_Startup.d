06214ceb232e2340687b97b8c2d73ce998e91ab1
Comandante <minamonra@gmail.com>
2024-02-21 18:15:37 +0300

    up

---
+++ matr_font_sl_01/Output/Debug/Obj/matr_font_sl_01/SEGGER_THUMB_Startup.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/matr_font_sl_01/SEGGER_THUMB_Startup.o: \
+ X:\_github\font_test\matr_font_sl_01\SEGGER_THUMB_Startup.s
---
+++ matr_font_sl_01/Output/Debug/Obj/matr_font_sl_01/main.d
@@ -0,0 +1,9 @@
+Output/Debug/Obj/matr_font_sl_01/main.o: \
+  X:\_github\font_test\matr_font_sl_01\main.c \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ 8.10b\include\stdio.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ 8.10b\include\__SEGGER_RTL.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ 8.10b\include\__SEGGER_RTL_ConfDefaults.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ 8.10b\include\__SEGGER_RTL_Conf.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ 8.10b\include\__SEGGER_RTL_Arm_Conf.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ 8.10b\include\stdlib.h \
+  X:\_github\font_test\matr_font_sl_01\myfont.h
