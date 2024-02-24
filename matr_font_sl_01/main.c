9f2e67e0a237b0fcd2ed75899e39d0d489fa2869
Comandante <minamonra@gmail.com>
2024-02-24 13:48:23 +0300

    up

--- matr_font_sl_01/main.c
+++ matr_font_sl_01/main.c
@@ -4,42 +4,51 @@
 //#include "consolas_22_font.h"
 //#include "ubuntunums.h"
 #include "fonts/SixteenSegment24x36.h"
+#include "SixteenSegment16x24.h"
+#include "Arial_round_16x24.h"
 int main(void) 
 {
   rcc_sysclockinit();
   SysTick_Config(48000); // 1ms if HSI
   gpio_init();
   spi_init();
-  delay_ms(10);
-  //lcd7735_init(CBLUE0);
+  delay_ms(2000);
   st7735init(LANDSCAPE, CBLUE0);
   
-  unsigned char x = 32;
+  //unsigned char x = 32, width = 24, height = 36, lenght = 108;;
+  unsigned char x = 0, width = 16, height = 24, lenght = 48;
+  unsigned char X1=8, Y1=8, W1=24;
+  
+  //const unsigned char *Font = SixteenSegment16x24;
+  //const unsigned int  *Fidx = SixteenSegment16x24dx;
+  const unsigned char *Font = Arial_round_16x24;
+  const unsigned int  *Fidx = Arial_round_16x24idx;
+
   do // do main 
   { 
     if (count > ttms || ttms - count > 500) {
       
-      unsigned char X1=8, Y1=8, W1=24;
-      print_char_sl(   x,       X1,    Y1, 24, 36, 108, SixteenSegment24x36, SixteenSegment24x36idx, CORANGE,  CBLACK); 
-      print_char_sl( x+1,    X1+W1,    Y1, 24, 36, 108, SixteenSegment24x36, SixteenSegment24x36idx, CGRAY,    CBLACK);
-      print_char_sl( x+2,  X1+W1*2,    Y1, 24, 36, 108, SixteenSegment24x36, SixteenSegment24x36idx, CGREEN0,  CBLACK); 
-      print_char_sl( x+3,  X1+W1*3,    Y1, 24, 36, 108, SixteenSegment24x36, SixteenSegment24x36idx, CGREEN1,  CBLACK);
-      print_char_sl( x+4,  X1+W1*4,    Y1, 24, 36, 108, SixteenSegment24x36, SixteenSegment24x36idx, CYELLOW0, CBLACK); 
-      print_char_sl( x+5,  X1+W1*5,    Y1, 24, 36, 108, SixteenSegment24x36, SixteenSegment24x36idx, CMAGENTA, CBLACK);
       
-      print_char_sl( x+6,       X1, Y1+38, 24, 36, 108, SixteenSegment24x36, SixteenSegment24x36idx, CPURPLE,  CBLACK);
-      print_char_sl( x+7,    X1+W1, Y1+38, 24, 36, 108, SixteenSegment24x36, SixteenSegment24x36idx, CGREEN2,  CBLACK);
-      print_char_sl( x+8,  X1+W1*2, Y1+38, 24, 36, 108, SixteenSegment24x36, SixteenSegment24x36idx, CRED,     CBLACK);
-      print_char_sl( x+9,  X1+W1*3, Y1+38, 24, 36, 108, SixteenSegment24x36, SixteenSegment24x36idx, CGREEN3,  CBLACK);
-      print_char_sl( x+10, X1+W1*4, Y1+38, 24, 36, 108, SixteenSegment24x36, SixteenSegment24x36idx, CRED0,    CBLACK);
-      print_char_sl( x+10, X1+W1*5, Y1+38, 24, 36, 108, SixteenSegment24x36, SixteenSegment24x36idx, CRUBY,    CBLACK);
+      print_char_sl(   x,       X1,    Y1, width, height, lenght, Font, Fidx, CORANGE,  CBLACK); 
+      print_char_sl( x+1,    X1+W1,    Y1, width, height, lenght, Font, Fidx, CGRAY,    CBLACK);
+      print_char_sl( x+2,  X1+W1*2,    Y1, width, height, lenght, Font, Fidx, CGREEN0,  CBLACK); 
+      print_char_sl( x+3,  X1+W1*3,    Y1, width, height, lenght, Font, Fidx, CGREEN1,  CBLACK);
+      print_char_sl( x+4,  X1+W1*4,    Y1, width, height, lenght, Font, Fidx, CYELLOW0, CBLACK); 
+      print_char_sl( x+5,  X1+W1*5,    Y1, width, height, lenght, Font, Fidx, CMAGENTA, CBLACK);
+      
+      print_char_sl( x+6,       X1, Y1+38, width, height, lenght, Font, Fidx, CPURPLE,  CBLACK);
+      print_char_sl( x+7,    X1+W1, Y1+38, width, height, lenght, Font, Fidx, CGREEN2,  CBLACK);
+      print_char_sl( x+8,  X1+W1*2, Y1+38, width, height, lenght, Font, Fidx, CRED,     CBLACK);
+      print_char_sl( x+9,  X1+W1*3, Y1+38, width, height, lenght, Font, Fidx, CGREEN3,  CBLACK);
+      print_char_sl( x+10, X1+W1*4, Y1+38, width, height, lenght, Font, Fidx, CRED0,    CBLACK);
+      print_char_sl( x+11, X1+W1*5, Y1+38, width, height, lenght, Font, Fidx, CRUBY,    CBLACK);
 
-      print_char_sl( x+11,      X1, Y1+76, 24, 36, 108, SixteenSegment24x36, SixteenSegment24x36idx, CMAGENTA,  CBLACK);
-      print_char_sl( x+12,   X1+W1, Y1+76, 24, 36, 108, SixteenSegment24x36, SixteenSegment24x36idx, CYELLOW0,  CBLACK);
-      print_char_sl( x+13, X1+W1*2, Y1+76, 24, 36, 108, SixteenSegment24x36, SixteenSegment24x36idx, CGRAY,     CBLACK);
-      print_char_sl( x+14, X1+W1*3, Y1+76, 24, 36, 108, SixteenSegment24x36, SixteenSegment24x36idx, CYELLOW1,  CBLACK);
-      print_char_sl( x+15, X1+W1*4, Y1+76, 24, 36, 108, SixteenSegment24x36, SixteenSegment24x36idx, CGREEN0,   CBLACK);
-      print_char_sl( x+16, X1+W1*5, Y1+76, 24, 36, 108, SixteenSegment24x36, SixteenSegment24x36idx, CORANGE,   CBLACK);
+      print_char_sl( x+12,      X1, Y1+76, width, height, lenght, Font, Fidx, CMAGENTA,  CBLACK);
+      print_char_sl( x+13,   X1+W1, Y1+76, width, height, lenght, Font, Fidx, CYELLOW0,  CBLACK);
+      print_char_sl( x+14, X1+W1*2, Y1+76, width, height, lenght, Font, Fidx, CGRAY,     CBLACK);
+      print_char_sl( x+15, X1+W1*3, Y1+76, width, height, lenght, Font, Fidx, CYELLOW1,  CBLACK);
+      print_char_sl( x+16, X1+W1*4, Y1+76, width, height, lenght, Font, Fidx, CGREEN0,   CBLACK);
+      print_char_sl( x+17, X1+W1*5, Y1+76, width, height, lenght, Font, Fidx, CORANGE,   CBLACK);
 
 
       //print_char_temp(x, 20, 30, 24, 32, 96, ubuntu_nums, ubuntu_nums_index, CORANGE, CBLACK); 
@@ -48,7 +57,7 @@ int main(void)
       //print_char_temp(x, 20+24, 30, 24, 32, 96, InconsolaNum24x32, InconsolaNum24x32index, CBLACK, CWHITE0); 
       //print_char_temp(x, 0, 0, 18, 35, 90, font_consolas_22, consolas22index, CORANGE, CBLACK);
       count = ttms;
-      x++; if (x > 95) x = 32;
+      x++; if (x > 95) x = 0;
     }
   } while (1); // main do
 } // main
