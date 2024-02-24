09caeb3237cd3b1d09b38357ef4caf98a573206a
Comandante <minamonra@gmail.com>
2024-02-24 15:44:45 +0300

    consolas_22

--- matr_font_sl_01/main.c
+++ matr_font_sl_01/main.c
@@ -6,23 +6,32 @@
 #include "fonts/SixteenSegment24x36.h"
 #include "SixteenSegment16x24.h"
 #include "Arial_round_16x24.h"
+#include "consolas_22_font.h"
+#include "consolas_18_font.h"
 int main(void) 
 {
   rcc_sysclockinit();
   SysTick_Config(48000); // 1ms if HSI
   gpio_init();
   spi_init();
-  delay_ms(2000);
+  delay_ms(100);
   st7735init(LANDSCAPE, CBLUE0);
   
-  //unsigned char x = 32, width = 24, height = 36, lenght = 108;;
-  unsigned char x = 0, width = 16, height = 24, lenght = 48;
+  //unsigned char x = 32, width = 24, height = 36, lenght = 108;; // SixteenSegment24x36
+  //unsigned char x = 0, width = 16, height = 24, lenght = 48;// SixteenSegment16x24 Arial_round_16x24
+  //unsigned char x =32, width = 15, height = 26, lenght = 52;  // consolas 22
+  unsigned char x =32, width = 18, height = 34, lenght = 102;  // consolas 22
   unsigned char X1=8, Y1=8, W1=24;
   
   //const unsigned char *Font = SixteenSegment16x24;
   //const unsigned int  *Fidx = SixteenSegment16x24dx;
-  const unsigned char *Font = Arial_round_16x24;
-  const unsigned int  *Fidx = Arial_round_16x24idx;
+  //const unsigned char *Font = Arial_round_16x24;
+  //const unsigned int  *Fidx = Arial_round_16x24idx;
+  //const unsigned char *Font = font_consolas_18;
+  //const unsigned int  *Fidx = font_consolas_18idx;
+  const unsigned char *Font = font_consolas_22;
+  const unsigned int  *Fidx = font_consolas_22idx;
+
 
   do // do main 
   { 
